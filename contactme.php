0857cbdee4b80e63a26d766c794ac9b63279ba8b
bobo <>
2017-11-18 00:05:54 -0500

    revise design

--- contactme.php
+++ contactme.php
@@ -16,7 +16,7 @@
         }
 
         .jumbotron{
-            background-color:rgba(219, 166, 92, 0.5);
+            background-color:rgba(219, 166, 92, 0.8);
             text-align: center;
             letter-spacing: 1.5px;
             padding-top:80px;
